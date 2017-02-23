e063e88f7ab37f1b508aac62eae792fef2457222
NurlashKO <zh.nurlan96@gmail.com>
2017-02-23 18:37:42 +0600

    added login/registration/logout

--- auth_app/urls.py
+++ auth_app/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    url(r'^login/$', views.login_view, name='login'),
-    url(r'^logout_view/$', views.logout_view, name='logout'),
+        url(r'^register/$', views.register, name='register'),
+	url(r'^login/$', auth_views.login, {'template_name': 'auth_app/login.html'}, name='login'),
+	url(r'^logout/$', auth_views.logout, name='logout'),
 ]
