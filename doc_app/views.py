8d578855932a7fdd83357414538eada2b4684eee
MuslimBeibytuly <muslimbeibytuly@gmail.com>
2017-02-23 23:32:39 +0600

    MVP2

--- doc_app/views.py
+++ doc_app/views.py
@@ -7,6 +7,7 @@ from docx.enum.text import WD_PARAGRAPH_ALIGNMENT
 from doc_app.forms import ClaimForm
 from doc_app.models import Claim
 
+
 # Create your views here.
 @login_required(login_url='/auth/login/')
 def document_print_view(request, id=id):
@@ -14,10 +15,10 @@ def document_print_view(request, id=id):
     document = Document()
     document.add_paragraph('Декану ФИТ').alignment = WD_PARAGRAPH_ALIGNMENT.RIGHT
     document.add_paragraph('Акжаловой А. Ж.').alignment = WD_PARAGRAPH_ALIGNMENT.RIGHT
-    document.add_paragraph(str.format('ФИТ, {} {} курса', request.user.speciality, request.user.study_year))\
+    document.add_paragraph(str.format('ФИТ, {} {} курса', request.user.speciality, request.user.study_year)) \
         .alignment = WD_PARAGRAPH_ALIGNMENT.RIGHT
-    document\
-        .add_paragraph(str.format("{} {} \n\n\n", request.user.first_name, request.user.last_name))\
+    document \
+        .add_paragraph(str.format("{} {} \n\n\n", request.user.first_name, request.user.last_name)) \
         .alignment = WD_PARAGRAPH_ALIGNMENT.RIGHT
     # document.add_paragraph('')
     # document.add_paragraph('')
@@ -48,6 +49,7 @@ def document_create_view(request):
     }
     context = context_dict.items()
     return render(request, "doc_app/document_create.html", context)
+
 
 @login_required(login_url='/auth/login/')
 def document_sign_view(request):
